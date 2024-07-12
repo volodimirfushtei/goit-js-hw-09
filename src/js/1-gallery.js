829c1c843a66d374a489783d48a596454ce30fb7
Volodimir <161057867+volodimirfushtei@users.noreply.github.com>
2024-07-12 17:07:19 +0300

    add

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -96,7 +96,7 @@ images.forEach(image => {
   fragment.appendChild(li);
 });
 gallery.appendChild(fragment);
-// Описаний в документації
+
 import SimpleLightbox from 'simplelightbox';
 const lightbox = new SimpleLightbox('.gallery a');
 import 'simplelightbox/dist/simple-lightbox.min.css';
